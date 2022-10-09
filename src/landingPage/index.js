b60a514e7f14b04e1937a36f7cc758f921446a6d
Uashington <uashington.sm@gmail.com>
2022-10-09 20:02:48 -0300

    Estilização pronta

--- src/landingPage/index.js
+++ src/landingPage/index.js
@@ -15,22 +15,22 @@ export default function LandingPage() {
                 </div>
             </header>
 
-            <section></section>
+            <section className="secao-banner"></section>
 
             <section>
-                <div>
+                <div className="container-bemVindo limitar-secao">
                     <h1>Bem-vindo a Barber Shop</h1>
-                    <p>
+                    <p className="pBemVindo"><span>
                         Nossa barbearia sempre oferece profissionais de qualidade e
                         estamos prontos para lidar com suas maiores expectativas.
-                    </p>
-                    <p>
+                    </span></p>
+                    <p className="pBemVindo">
                         Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos
                         uma equipe premiada que demonstrou o talento de mestres barbeiros
                         em vários concursos de estilo. Deixe nosso barbeiro ser seu
                         estilista pessoal e você nunca ficará desapontado.
                     </p>
-                    <p>S. Kelly</p>
+                    <p className="assinatura">S. Kelly</p>
                 </div>
             </section>
         </div>
